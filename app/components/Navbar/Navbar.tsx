43eba7836f805ee4194c7b4f70ca5b49433a15f5
Isaladwy <Isaladwy@gmail.com>
2025-07-03 15:45:58 +0300

    Update Navbar component to refine text sizes for improved readability and responsiveness across various devices.

--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -12,18 +12,18 @@ const Navbar = () => {
   return (
     <nav
       dir="rtl"
-      className="sticky top-0 z-50 w-full bg-neutral-950 bg-opacity-95 shadow-lg border-b border-orange-500/20"
+      className="sticky top-0 z-50 text-l w-full bg-neutral-950 bg-opacity-95 shadow-lg border-b border-orange-500/20"
     >
       <div className="max-w-7xl mx-auto flex items-center justify-between px-6 py-3">
         {/* Site Name/Logo */}
         <Link
           href="#hero"
-          className="md:text-2xl text-xl font-extrabold text-orange-400 tracking-tight select-none"
+          className="md:text-2xl text-l font-extrabold text-orange-400 tracking-tight select-none"
         >
           كاميرات الرؤية
         </Link>
         {/* Nav Links */}
-        <ul className="flex gap-6 md:gap-10 items-center text-lg font-semibold">
+        <ul className="flex gap-6 md:gap-10 items-center text-sm font-semibold">
           {navLinks.map((link) => (
             <li key={link.href}>
               <Link
