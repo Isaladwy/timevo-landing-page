3e5d55f1bcaf6aacab66037c51e9bb5cee108786
Isaladwy <Isaladwy@gmail.com>
2025-07-03 16:19:42 +0300

    Refactor Navbar and Testimonials components to improve text sizes and layout for better readability and consistency across devices.

--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -12,18 +12,18 @@ const Navbar = () => {
   return (
     <nav
       dir="rtl"
-      className="sticky top-0 z-50 text-l w-full bg-neutral-950 bg-opacity-95 shadow-lg border-b border-orange-500/20"
+      className="sticky top-0 z-50 text-sm w-full bg-neutral-950 bg-opacity-95 shadow-lg border-b border-orange-500/20"
     >
-      <div className="max-w-7xl mx-auto flex items-center justify-between px-6 py-3">
+      <div className="max-w-7xl mx-auto flex items-center justify-between px-3 py-3">
         {/* Site Name/Logo */}
         <Link
           href="#hero"
-          className="md:text-2xl text-l font-extrabold text-orange-400 tracking-tight select-none"
+          className="md:text-2xl text-xs font-extrabold text-orange-400 tracking-tight select-none"
         >
           كاميرات الرؤية
         </Link>
         {/* Nav Links */}
-        <ul className="flex gap-6 md:gap-10 items-center text-sm md:text-2xl font-semibold">
+        <ul className="flex gap-6 md:gap-10 items-center justify-center text-xs md:text-2xl ">
           {navLinks.map((link) => (
             <li key={link.href}>
               <Link
